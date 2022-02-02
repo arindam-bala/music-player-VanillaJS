802db495242a4dee49516d54826b862a7217e9e2
arindam-bala <arindambala576@gmail.com>
2022-02-02 20:58:22 +0530

    adding id "cover" in image src

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const musicContainer = document.querySelector('music-container');
+const musicContainer = document.querySelector('.music-container');
 const playBtn = document.querySelector('#play');
 const prevBtn = document.querySelector('#prev');
 const nextBtn = document.querySelector('#next');
@@ -6,3 +6,42 @@ const audio = document.querySelector('#audio');
 const progress = document.querySelector('.progress');
 const progressContainer = document.querySelector('.progress-container');
 const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+//Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+//Keep track of songs
+let songIndex = 1;
+
+//Initially load song info into the DOM
+loadSong(songs[songIndex]);
+
+//Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `image/${song}.jpg`;
+}
+
+function playSong() {
+    musicContainer.classList.add('play');
+}
+
+function pauseSong() {
+
+}
+
+//Event Listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+        
+})
+
+
